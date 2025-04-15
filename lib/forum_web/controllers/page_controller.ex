1549ced49a6b844e0c80374259611909f2565fbc
HarishKumarT3547 <harishk1@asu.edu>
2025-04-15 12:43:19 -0700

    API changes
    used command "mix phx.gen.json Posts Post posts body:string title:string" for creating an api functionality.
    1 Posts is name of the context which is like modal in MVC
    2 Post is name of the schema
    3 posts is name of the table name
    
    By running this it gave a command which is to be added in the api part of the router
    
    And one more command is given which will allow use to create the table in the db

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,16 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Bob", email: "bob@email.com"},
+      %{id: 3, name: "Charlie", email: "charlie@email.com"},
+      %{id: 4, name: "Dan", email: "dan@email.com"},
+      %{id: 5, name: "Eve", email: "eve@email.com"}
+    ]
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
